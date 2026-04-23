288c061533f90753a55544f1f4fd16342465c949
SUJITH KUMAR R <sujitvskumar8@gmail.com.com>
2026-04-23 21:03:13 +0530

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Secure Docker CI/CD App Running!"
+    return "Secure Docker CI/CD App Running! by sujith"
 
 @app.route('/health')
 def health():
